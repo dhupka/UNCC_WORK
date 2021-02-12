7f7f4e41a75563fd38e5d53c6e8da8475cefd241
dhupka <dhupka1@gmail.com>
2021-02-12 17:50:34 -0500

    fix

--- serverLatency2.py
+++ serverLatency2.py
@@ -7,8 +7,8 @@ import csv
 #Creating the constants that we are using 
 message = "Strong storm coming, pack up and leave, 5 minutes"
 HOST = '192.168.0.167' # IP address of server
-PORT = 9007
-PORT2 = 8007
+PORT = 9001
+PORT2 = 8001
 
 async def gettingClientConnection(host,port,socket):
 	socket.connect((host, port))
@@ -27,7 +27,7 @@ async def recievingDataServer(connection , size):
     message = data.decode('utf-8' , 'replace')
     sizeOfMessage = sys.getsizeof(message)
     # print("Size of Message: {0}".format(sys.getsizeof(message)))
-    # print("Received Message: ", message[2:]) #Removing tab and ascii character that is added for some reason TODO
+    #print("Received Message: ", message[2:]) #Removing tab and ascii character that is added for some reason TODO
 
 async def recievingDataServerFirstTime(connection):
     data = connection.recv(1024)
@@ -48,10 +48,15 @@ async def send_msg1(connection , msg):
         connection.sendall(msg_utf)
 
 async def main():
+    
+    
+    
     s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s1.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s2.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-	
+
     socket1 = await startingSocketServer(HOST,PORT,s1)
     socket2 = await startingSocketServer(HOST,PORT2,s2)
 
@@ -63,7 +68,7 @@ async def main():
     iteration = 0	
     with open('latency-Scenario1-10m.csv','w') as f1:
         writer=csv.writer(f1, delimiter='\t',lineterminator='\n',)
-        while iteration < 2000:
+        while iteration < 1000:
             # if iteration == 0:
             #     await send_msg1(connection1 , message)
             #     print('Message Sent socket 1 first time')
@@ -94,7 +99,7 @@ async def main():
             print("Recieved data from connection 2 ")
             #print('message receieve sock 2')		
             rtt = ((time.time() - time0) * 1000)/2
-            print("Total Latency2: {:.2f} ms".format(rtt))
+            print("Total Latency: {:.2f} ms".format(rtt))
             row = [rtt]
             writer.writerow(row)
             iteration = iteration + 1
